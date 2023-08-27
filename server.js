d1372f753ed3d0777fc0c4930a244e596d591996
lesoRoman <ladiginscormag@gmail.com>
2023-08-27 12:02:24 +0300

    create

--- server.js
+++ server.js
@@ -4,28 +4,15 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 
-const users = [
-	{ id: 1, username: 'john_doe', access: true },
-	// Додайте інші користувачі тут
-]
+
 
 app.post('/getUserInfo', (req, res) => {
 	const { username } = req.body
 
-	const user = users.find(u => u.username === username)
-	if (user) {
-		const userInfo = {
-			id: user.id,
-			username: user.username,
-			access: true,
-		}
-		res.json(userInfo)
-	} else {
-		res.status(404).json({ error: 'User not found' })
-	}
+	res.json(username)
 })
 
-const port = 3000
+const port = 8888
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`)
 })
