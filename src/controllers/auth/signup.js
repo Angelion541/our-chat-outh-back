796f155b404dc622d20745e58d0559ed4d9b67ab
lesoRoman <ladiginscormag@gmail.com>
2023-08-28 18:37:49 +0300

    some bag fix

--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -2,36 +2,36 @@
 // const bcrypt = require("bcrypt");
 
 async function signup(req, res) {
-  const { userName } = req.body;
-  // const emailToLoWerCase = email.toLowerCase();
+	const { name } = req.body
+	// const emailToLoWerCase = email.toLowerCase();
 
-  // const userCheck = await User.findOne({ email: emailToLoWerCase });
-  // if (userCheck) {
-  //   res.status(409).json({ message: "Email in use" });
-  //   return;
-  // }
+	// const userCheck = await User.findOne({ email: emailToLoWerCase });
+	// if (userCheck) {
+	//   res.status(409).json({ message: "Email in use" });
+	//   return;
+	// }
 
-  // const salt = await bcrypt.genSalt(10);
-  // const hashedPassword = await bcrypt.hash(password, salt);
+	// const salt = await bcrypt.genSalt(10);
+	// const hashedPassword = await bcrypt.hash(password, salt);
 
-  // const newUser = await User.create({
-  //   email: emailToLoWerCase,
-  //   password: hashedPassword,
-  //   phone,
-  //   birthday,
-  //   name,
-  //   avatarUrl,
-  //   city,
-  // });
-  // const { userName } = req.body;
-  // const  userName  = 'ROMA';
-  return res.status(201).json({
+	// const newUser = await User.create({
+	//   email: emailToLoWerCase,
+	//   password: hashedPassword,
+	//   phone,
+	//   birthday,
+	//   name,
+	//   avatarUrl,
+	//   city,
+	// });
+	// const { userName } = req.body;
+	// const  userName  = 'ROMA';
+	return res.status(201).json({
 		status: 'success',
 		code: 201,
 		user: {
-			userName,
+			userName: name,
 		},
-  })
+	})
 }
 
-module.exports = signup;
+module.exports = signup
