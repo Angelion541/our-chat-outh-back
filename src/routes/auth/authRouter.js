9a9d9ec60f3c30ed55c747da66b3553abc645fbd
lesoRoman <ladiginscormag@gmail.com>
2023-09-09 14:15:55 +0300

    some bag fix

--- src/routes/auth/authRouter.js
+++ src/routes/auth/authRouter.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const { authController } = require("../../controllers");
-// const { authMiddleware } = require("../../middlewares/authMiddleware");
 const { ctrlWrapper } = require("../../middlewares/ctrlWrapper");
 
 const authRouter = express.Router();
 
-authRouter.post("/signup", ctrlWrapper(authController.signup));
+authRouter.post("/signup",  ctrlWrapper(authController.signup));
 
 module.exports = authRouter;
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -9,37 +9,15 @@ const schema = mongoose.Schema(
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
       unique: true,
       match: [/[a-z0-9]+@[a-z0-9]+/, "user email is not valid!"],
     },
     password: {
       type: String,
-      required: [true, "password is required"],
     },
-    birthday: {
-      type: Date,
-      default: null,
-    },
-    phone: {
+    mood: {
       type: String,
-      default: null,
-    },
-    city: {
-      type: String,
-      default: "",
-    },
-    avatarUrl: {
-      type: String,
-      default: "",
-    },
-    accessToken: {
-      type: String,
-      default: null,
-    },
-    refreshToken: {
-      type: String,
-      default: null,
+      enum: ["1", "2","3","4","5"],
     },
     userLikePets: [{ type: Schema.Types.ObjectId, ref: "notice" }],
     userAddPet: [{ type: Schema.Types.ObjectId, ref: "userAddPet" }],
