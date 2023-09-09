5ef9bfea90be9eb3c87106c9235bf9e117fd5d3b
lesoRoman <ladiginscormag@gmail.com>
2023-09-09 14:36:39 +0300

    som delete

--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -1,30 +1,6 @@
-// const { User } = require("../../schemas/user");
-// const bcrypt = require("bcrypt");
 
 async function signup(req, res) {
 	const { userName, userMood } = req.body
-	// const emailToLoWerCase = email.toLowerCase();
-
-	// const userCheck = await User.findOne({ email: emailToLoWerCase });
-	// if (userCheck) {
-	//   res.status(409).json({ message: "Email in use" });
-	//   return;
-	// }
-
-	// const salt = await bcrypt.genSalt(10);
-	// const hashedPassword = await bcrypt.hash(password, salt);
-
-	// const newUser = await User.create({
-	//   email: emailToLoWerCase,
-	//   password: hashedPassword,
-	//   phone,
-	//   birthday,
-	//   name,
-	//   avatarUrl,
-	//   city,
-	// });
-	// const { userName } = req.body;
-	// const  userName  = 'ROMA';
 	return res.status(201).json({
 		code: 201,
 		data: {
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,48 +1,22 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
 
 const schema = mongoose.Schema(
   {
-    name: {
+    userName: {
       type: String,
       default: "",
+    },
+    userMood: {
+      type: String,
+      enum: ["1", "2","3","4","5"]
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
       unique: true,
-      match: [/[a-z0-9]+@[a-z0-9]+/, "user email is not valid!"],
     },
     password: {
       type: String,
-      required: [true, "password is required"],
-    },
-    birthday: {
-      type: Date,
-      default: null,
-    },
-    phone: {
-      type: String,
-      default: null,
-    },
-    city: {
-      type: String,
-      default: "",
-    },
-    avatarUrl: {
-      type: String,
-      default: "",
-    },
-    accessToken: {
-      type: String,
-      default: null,
-    },
-    refreshToken: {
-      type: String,
-      default: null,
-    },
-    userLikePets: [{ type: Schema.Types.ObjectId, ref: "notice" }],
-    userAddPet: [{ type: Schema.Types.ObjectId, ref: "userAddPet" }],
+    }
   },
   { versionKey: false, timestamps: true }
 );
