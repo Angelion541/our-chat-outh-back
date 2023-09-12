cdae156082b35ed37e6d004340fc4db2cb2f8a70
Roman lesyo <ladiginscormag@gmail.com>
2023-09-12 17:40:25 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,25 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const authRouter = require("./routes/auth/authRouter");
+const authRouter = require('./routes/auth/authRouter')
 
+const app = express()
 
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-const app = express();
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
 
-
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-app.use(logger(formatsLogger));
-app.use(cors());  
-app.use(express.json());
-
-app.use("/auth", authRouter);
+app.use('/auth', authRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
+	res.status(404).json({ message: 'Not found' })
+})
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
-});
+	res.status(500).json({ message: err.message })
+})
 
-module.exports = app;
+module.exports = app
