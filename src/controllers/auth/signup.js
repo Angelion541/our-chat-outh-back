40381d3385c6800a8cdf4947d462bab51db17ab2
Roman lesyo <ladiginscormag@gmail.com>
2023-09-12 16:15:29 +0300

    some bag fix

--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -1,12 +1,17 @@
+const  User  = require('../../models/user')
 
 async function signup(req, res) {
 	const { userName, userMood } = req.body
+	console.log(userName, userMood)
+
+	const newUser = await User.create({
+		userName,
+		userMood,
+	})
+
 	return res.status(201).json({
 		code: 201,
-		data: {
-			userMood,
-			userName,
-		},
+		newUser,
 	})
 }
 
